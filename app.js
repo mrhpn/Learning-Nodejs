1e5a7db278c24cbe34cc81cc2b76eea4d5b5e2cd
Htet Phyo Naing <mailtohtetphyonaing@gmail.com>
2020-02-23 14:31:51 +0630

    second commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+/**
+ * 1 - Middleware
+ */
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+/**
+ * 3 - Routes
+ */
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
